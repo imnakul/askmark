0101f20e8084b2d7013d2643f4ea7b0e49e5c2c5
Nakul Srivastava <imnakul44@gmail.com>
2025-05-15 21:09:20 +0530

    little size change in navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar({ QR, setQR }) {
    const router = useRouter()
 
    return (
-      <div className='navbar bg-white/5 shadow-sm max-w-7xl w-full mx-auto rounded-md flex items-center justify-between'>
+      <div className='navbar bg-white/10 shadow-sm max-w-7xl w-full mx-auto rounded-md flex items-center justify-between'>
          <div className='flex items-center gap-0 cursor-pointer'>
             <img src='/logo.jpg' alt='logo' className='size-12' />
             <span
@@ -19,7 +19,7 @@ function Navbar({ QR, setQR }) {
 
          <div className='flex items-center gap-4 justify-center px-2 py-1'>
             <div className='avatar'>
-               <div className='ring-primary ring-offset-base-100 w-10 rounded-full ring-2 ring-offset-2'>
+               <div className='ring-primary ring-offset-base-100 w-8 rounded-full ring-2 ring-offset-2'>
                   <img src='https://img.daisyui.com/images/profile/demo/spiderperson@192.webp' />
                </div>
             </div>
